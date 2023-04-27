6ba764b5a3c184929064e915f6cbbbd54553328c
crismogram <crismontimpog@hotmail.com>
2023-04-28 05:07:43 +0800

    Add Debug files

--- dotnet/DesignPatterns/Program.cs
+++ dotnet/DesignPatterns/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Thread t1 = new Thread(() => {
+    var instance = UploadService.GetInstance(1);
+});
+
+Thread t2 = new Thread(() => {
+    var instance = UploadService.GetInstance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+System.Console.WriteLine("Hello, World!");
