6f5010ece0095bede772bbed37a145d20932b141
crismogram <crismontimpog@hotmail.com>
2023-04-28 06:29:09 +0800

    More on Singleton

--- dotnet/DesignPatterns/Program.cs
+++ dotnet/DesignPatterns/Program.cs
@@ -1,17 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Thread t1 = new Thread(() => {
-    var instance = UploadService.GetInstance(1);
-});
 
-Thread t2 = new Thread(() => {
-    var instance = UploadService.GetInstance(2);
-});
+// ---------------------------------------------------------------------------------------------
+class Program {
+static TableServers host1List = TableServers.GetTableServers();
+static TableServers host2List = TableServers.GetTableServers();
 
-t1.Start();
-t2.Start();
+    static void Main(string[] args){
+        TableServers servers = TableServers.GetTableServers();
 
-t1.Join();
-t2.Join();
+        for (int i = 0; i < 5; i++)
+        {
+            Host1GetNextServer();
+            Host2GetNextServer();
+        }
 
-System.Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+
+    private static void Host1GetNextServer(){
+        System.Console.WriteLine($"Host 1: The next server is: {host1List.GetNextServer()}");
+    }
+    private static void Host2GetNextServer(){
+        System.Console.WriteLine($"Host 2: The next server is: {host2List.GetNextServer()}");
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+
+// Singleton Upload Service
+// Thread t1 = new Thread(() => {
+//     var instance = UploadService.GetInstance(1);
+// });
+
+// Thread t2 = new Thread(() => {
+//     var instance = UploadService.GetInstance(2);
+// });
+
+// t1.Start();
+// t2.Start();
+
+// t1.Join();
+// t2.Join();
+
+// ---------------------------------------------------------------------------------------------
